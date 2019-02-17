4a47e2146aec7e707bf8aa8664b65a5ba284d35e
Your Name <you@example.com>
2019-02-18 00:36:22 +0800

    clean up

--- application/index/behavior/loadConfig.php
+++ application/index/behavior/loadConfig.php
@@ -7,10 +7,10 @@ class loadConfig
 {
 	public function run()
 	{
-		/*if(!$configs = Cache::get('config'))
+		if(!$configs = Cache::get('config'))
 		{
 			$configs = Config::column('value', 'name');
 			Cache::set('config', $configs);//永久缓存 除非后台更新设置
-		}*/
+		}
 	}
 }
